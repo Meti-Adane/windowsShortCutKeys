140da1a5f2a5754d418a283cf2e3b21ca642fbb2
Meti A <metiade7@gmail.com>
2021-07-29 21:20:31 +0300

    Quiz and reading feature

---
+++ data.js
@@ -0,0 +1,98 @@
+windowsshortkey = {0: ['Win + F1', 'Open Windows Help and support'], 1: ['Win', 'Display or hide the Start screen'], 2: ['Win + B', 'Set focus in the notification area'], 3: ['Win + C', 'Open Charms'], 4: ['Win + D', 'Display and hide the desktop'], 5: ['Win + E', 'Open File Explorer'], 6: ['Win + F', 'Open the Search charm and search for files'], 7: ['Win + H', 'Open the Share charm'], 8: ['Win + I', 'Open the Settings charm'], 9: ['Win + K', 'Open the Devices charm'], 10: ['Win + L', 'Lock your PC or switch people'], 11: ['Win + M', 'Minimize all windows'], 12: ['Win + O', 'Lock device orientation'], 13: ['Win + P', 'Choose a presentation display mode'], 14: ['Win + Q', 'Open the Search charm to search everywhere or within the open app (if the app supports app search)'], 15: ['Win + R', 'Open the Run dialog box'], 16: ['Win + S', 'Open the Search charm to search Windows and the web'], 17: ['Win + T', 'Cycle through apps on the taskbar'], 18: ['Win + U', 'Open Ease of Access Center'], 19: ['Win + V', 'Cycle through notifications'], 20: ['Win + SHIFT + V', 'Cycle through notifications in reverse order'], 21: ['Win + W', 'Open the Search charm and search for settings'], 22: ['Win + X', 'Open the Quick Link menu'], 23: ['Win + Z', 'Show the commands available in the app'], 24: ['Win + ,', 'Temporarily peek at the desktop'], 25: ['Win + PAUSE', 'Display the System Properties dialog box'], 26: ['Win + CTRL + F', 'Search for PCs (if you’re on a network)'], 27: ['Win + SHIFT + M', 'Restore minimized windows on the desktop'], 28: ['Win + (NUMBER 1-9)', 'Open the desktop and start the app pinned to the taskbar in the position indicated by the number. If the app is already running, it switches to that app.'], 29: ['Win + SHIFT + (NUMBER 1-9)', 'Open the desktop and start a new instance of the app pinned to the taskbar in the position indicated by the number'], 30: ['Win + CTRL + (NUMBER 1-9)', 'Open the desktop and switch to the last active window of the app pinned to the taskbar in the position indicated by the number'], 31: ['Win + ALT + (NUMBER 1-9)', 'Open the desktop and open the Jump List for the app pinned to the taskbar in the position indicated by the number'], 32: ['Win + CTRL + SHIFT + (NUMBER 1-9)', 'Open the desktop and open a new instance of the app located at the given position on the taskbar as an administrator'], 33: ['Win + TAB', 'Cycle through recently used apps (except desktop apps)'], 34: ['Win + CTRL + TAB', 'Cycle through recently used apps (except desktop apps)'], 35: ['Win + SHIFT + TAB', 'Cycle through recently used apps (except desktop apps) in reverse order'], 36: ['Win + CTRL + B', 'Switch to the app that displayed a message in the notification area'], 37: ['Win + UP ARROW', 'Maximize the window'], 38: ['Win + DOWN ARROW', 'Remove current app from screen or minimize the desktop window'], 39: ['Win + LEFT ARROW', 'Maximize the app or desktop window to the LEFT side of the screen'], 40: ['Win + RIGHT ARROW', 'Maximize the app or desktop window to the RIGHT side of the screen'], 41: ['Win + HOME', 'Minimize all but the active desktop window (restores all windows on second stroke)'], 42: ['Win + SHIFT + UP ARROW', 'Stretch the desktop window to the top and bottom of the screen'], 43: ['Win + SHIFT + DOWN ARROW', 'Restore/minimize active desktop windows vertically, maintaining width'], 44: ['Win + SHIFT + LEFT ARROW or RIGHT ARROW', 'Move an app or window in the desktop from one monitor to another'], 45: ['Win + SPACEBAR', 'Switch input language and keyboard layout'], 46: ['Win + CTRL + SPACEBAR', 'Change to a previously selected input'], 47: ['Win + ENTER', 'Open Narrator'], 48: ['Win + SHIFT + .', 'Cycle through open apps'], 49: ['Win + .', 'Cycle through open apps'], 50: ['Win + /', 'Initiate IME reconversion'], 51: ['Win + ALT + ENTER', 'Open WindowsMedia Center'], 52: ['Win + +/-', 'Zoom in or out using Magnifier'], 53: ['Win + ESC', 'Exit Magnifier']}
+generalshort = {0: ['F1 ', 'Display Help'], 1: ['F2 ', 'Rename the selected item'], 2: ['F3 ', 'Search for a file or folder'], 3: ['F4 ', 'Display the  address  bar list in File Explorer'], 4: ['F5 ', 'Refresh the active window'], 5: ['F6 ', 'Cycle through screen elements in a window or on the desktop'], 6: ['F10 ', 'Activate the Menu bar in the active app'], 7: ['ALT  +  F4 ', 'Close the active item, or exit the active app'], 8: ['ALT  +  ESC ', 'Cycle through items in the order in which they were opened'], 9: ['ALT  +  Underlined Letter  in menus and dialog box options', 'Perform the command for that letter'], 10: ['ALT  +  ENTER ', 'Display properties for the selected item'], 11: ['ALT  +  SPACEBAR ', 'Open the shortcut menu for the active window'], 12: ['ALT  +  LEFT ARROW ', 'Back'], 13: ['ALT  +  RIGHT ARROW ', 'Forward'], 14: ['ALT  +  PAGE UP ', 'Move up one screen'], 15: ['ALT  +  PAGE DOWN ', 'Move down one screen'], 16: ['ALT  +  TAB ', 'Switch between open apps (except desktop apps)'], 17: ['CTRL  +  F4 ', 'Close the active document (in apps that allow you to have multiple documents open simultaneously)'], 18: ['CTRL  +  A ', 'Select all items in a document or window'], 19: ['CTRL  +  C  or  CTRL  +  INSERT ', 'Copy the selected item'], 20: ['CTRL  +  D / DELETE ', 'DELETE the selected item and move it to the Recycle Bin'], 21: ['CTRL  +  R  or  F5 ', 'Refresh the active window'], 22: ['CTRL  +  V  or  SHIFT  +  INSERT ', 'Paste the selected item'], 23: ['CTRL  +  X ', 'Cut the selected item'], 24: ['CTRL  +  Y ', 'Redo an action'], 25: ['CTRL  +  Z ', 'Undo an action'], 26: ['CTRL  +  +  or  CTRL  +  – ', 'Zoom in or out of a large NUMBER of items, like apps pinned to the Start screen'], 27: ['CTRL  + mouse scroll wheel', 'Change the size of  desktop  icons or zoom in or out of a large NUMBER of items, like apps pinned to the Start screen'], 28: ['CTRL  +  RIGHT ARROW ', 'Move the cursor to the beginning of the next word'], 29: ['CTRL  +  LEFT ARROW ', 'Move the cursor to the beginning of the previous word'], 30: ['CTRL  +  DOWN ARROW ', 'Move the cursor to the beginning of the next paragraph'], 31: ['CTRL  +  UP ARROW ', 'Move the cursor to the beginning of the previous paragraph'], 32: ['CTRL  +  ALT  +  TAB ', 'Use the arrow keys to switch between all open apps'], 33: ['CTRL  +  ARROW  +  SPACEBAR ', 'Select multiple individual items in a window or on the desktop'], 34: ['CTRL  +  SHIFT  +  ARROW ', 'Select a block of text'], 35: ['CTRL  +  ESC ', 'Open the Start screen'], 36: ['CTRL  +  SHIFT  +  ESC ', 'Open Task Manager'], 37: ['CTRL  +  SHIFT ', 'Switch the keyboard layout when multiple keyboard layouts are available'], 38: ['CTRL  +  SPACEBAR ', 'Turn the Chinese input method editor (IME) on or off'], 39: ['SHIFT  +  F10 ', 'Display the shortcut  menu  for the selected item'], 40: ['SHIFT  +  ARROW ', 'Select more than one item in a window or on the desktop, or select  text  within a document'], 41: ['SHIFT  +  DELETE ', 'Delete the selected item without moving it to the  Recycle  Bin first'], 42: ['RIGHT ARROW ', 'Open the next menu to the right, or open a submenu'], 43: ['LEFT ARROW ', 'Open the next menu to the left, or close a submenu'], 44: ['ESC ', 'Stop or leave the current task']}
+dialogshort = {0: ['F1', 'Display Help'], 1: ['F4', 'Display the items in the active list'], 2: ['CTRL + TAB', 'Move forward through tabs'], 3: ['CTRL + SHIFT + TAB', 'Move back through tabs'], 4: ['CTRL + (NUMBER 1-9)', 'Move to nth tab'], 5: ['TAB', 'Move forward through options'], 6: ['SHIFT + TAB', 'Move back through options'], 7: ['ALT + Underlined Letter in menus and dialog box options', 'Perform the command (or select the option) that goes with that letter'], 8: ['SPACEBAR', 'Select or clear the check box if the active option is a check box'], 9: ['BACKSPACE', 'Open a folder one level up if a folder is selected in the Save As or Open dialog box'], 10: ['ARROW keys', 'Select a button if the active option is a group of option buttons']}
+
+
+nextBtn = document.querySelector("#nextbtn")
+prevBtn = document.querySelector("#prevbtn")
+
+everythingBtn = document.querySelector("#everythingbtn")
+generalbtn = document.querySelector("#generalbtn")
+winbtn = document.querySelector("#winbtn")
+dialogbtn = document.querySelector("#dialogbtn")
+shufflebtn  = document.querySelector("#shufflebtn")
+
+questionBar = document.querySelector(".card-title")
+descbar = document.querySelector(".card-text")
+card = document.querySelector(".card")
+
+shown = []
+
+
+dataNum = 0
+
+function showData(datachoosen, dataNum) {
+    console.log(dataNum)
+    if (dataNum >= 0 && dataNum < Object.keys(datachoosen).length){
+        questionBar.innerText = datachoosen[dataNum][0]
+
+        descbar.classList.remove('isVisible')
+        descbar.innerText = datachoosen[dataNum][1]        
+    }
+    
+    
+};
+
+dataChoosen = {
+    ...windowsshortkey,
+    ...dialogshort,
+    ...generalshort
+}
+
+winbtn.addEventListener('click', e=>{
+    e.preventDefault()
+    dataChoosen = windowsshortkey
+    dataNum = 0
+    
+    showData(dataChoosen, dataNum)
+});
+dialogbtn.addEventListener('click', e=>{
+    e.preventDefault()
+    dataChoosen = dialogshort
+    dataNum = 0
+    
+    showData(dataChoosen, dataNum)
+});
+generalbtn.addEventListener('click', e=>{
+    e.preventDefault()
+    dataChoosen = generalshort
+    dataNum = 0
+    
+    showData(dataChoosen, dataNum)
+});
+
+shufflebtn.addEventListener('click', e=>{
+    e.preventDefault()
+    dataChoosen = {
+        ...windowsshortkey,
+        ...dialogshort,
+        ...generalshort
+    } 
+    dataNum = 0
+    
+    showData(dataChoosen, dataNum)
+});
+
+
+
+showData(dataChoosen, dataNum)
+nextBtn.addEventListener('click', e=>{
+    if (dataNum == Object.keys(dataChoosen).length - 1){
+        dataNum = 0
+    }else{
+        dataNum += 1
+    }
+    
+    showData(dataChoosen, dataNum)
+})
+prevBtn.addEventListener('click', e=>{
+    if (dataNum <= 0){
+        dataNum = Object.keys(dataChoosen).length - 1
+    }else{
+        dataNum -= 1
+    }
+    
+    showData(dataChoosen, dataNum)
+})
+
+
